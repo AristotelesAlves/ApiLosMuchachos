d812f04da60075614bc4066f1a61d34d42621854
Aristóteles <arystotelys@gmail.com>
2023-02-15 10:11:22 -0300

    Migrate init

--- src/server.ts
+++ src/server.ts
@@ -3,4 +3,6 @@ import express from 'express';
 const app = express();
 const port = 5432
 
-app.listen(port, () => { console.log(`Sering is running on port ${port}`); });
+app.listen(port, () => { 
+    console.log(`Sering is running on port ${port}`);   
+ });
