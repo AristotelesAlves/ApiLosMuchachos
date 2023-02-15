83add55613f6f95a7b94878cf302cec6d88a11bc
Aristóteles <arystotelys@gmail.com>
2023-02-15 19:17:39 -0300

    Rotas da API /cozinha /cixa /bar

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,11 @@
 import express from 'express';
+import { router } from './router';
 
 const app = express();
-const port = 5432
+const port = 3030
+
+app.use(express.json());
+app.use(router);
 
 app.listen(port, () => { 
     console.log(`Sering is running on port ${port}`);   
