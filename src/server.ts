b7e6d2cabd07443c95b8ee491ec2025d380f9bc2
Aristóteles <arystotelys@gmail.com>
2023-02-17 00:04:35 -0300

    Port server

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,7 @@ import { router } from './router';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = 3333;
 
 app.use(express.json());
 app.use(router);
